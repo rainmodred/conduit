5f6e396f0737b26fd59af23f92c259bb532587f9
rainmodred <albphoenix@gmail.com>
2022-08-11 03:20:19 +0300

    Refactor pagination

--- components/Shared/Pagination/usePagination.test.tsx
+++ components/Shared/Pagination/usePagination.test.tsx
@@ -1,6 +1,6 @@
-import { renderHook } from '@testing-library/react-hooks';
 import usePagination from './usePagination';
 import { useRouter } from 'next/router';
+import { renderHook } from '@testing-library/react';
 
 jest.mock('next/router', () => ({
   useRouter: jest.fn(),
@@ -15,24 +15,20 @@ describe('usePagination', () => {
     asPath: '/',
     query: '',
   }));
-
   it('should return range and activePage', () => {
     const { result } = renderHook(() => usePagination(5));
     const { range, activePage } = result.current;
     const expectedRange = [1, 2, 3, 4, 5];
-
     expect(range).toHaveLength(expectedRange.length);
     expect(range).toEqual(expectedRange);
     expect(activePage).toBe(1);
   });
-
   it('should work with query', () => {
     useRouter.mockImplementation(() => ({
       query: { page: 2 },
     }));
     const { result } = renderHook(() => usePagination(5));
     const { activePage } = result.current;
-
     expect(activePage).toBe(2);
   });
 });
