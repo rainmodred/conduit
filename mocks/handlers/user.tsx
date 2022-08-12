86f7d2ca7cc59b7a28aae02d075ed25ce3e8eaa2
rainmodred <albphoenix@gmail.com>
2022-08-12 05:13:25 +0300

    Refactor user handlers

--- mocks/handlers/user.tsx
+++ mocks/handlers/user.tsx
@@ -1,46 +1,43 @@
 import { rest } from 'msw';
 
+import { LoginFormValues } from '../../components/LoginForm/LoginForm';
+import { RegisterFormValues } from '../../components/RegisterForm/RegisterForm';
+
 import { API_URL } from '../../config/config';
-import db, { persistDb } from '../db';
-import { authenticate, delayedResponse, hash } from '../serverUtils';
+
+import db, { createUser, persistDb } from '../db';
+import { authenticate, delayedResponse } from '../serverUtils';
 
 type RegisterBody = {
-  user: {
-    email: string;
-    username: string;
-    password: string;
-  };
+  user: RegisterFormValues;
 };
 
 type LoginBody = {
-  user: {
-    email: string;
-    password: string;
-  };
+  user: LoginFormValues;
 };
 
 export const userHandlers = [
-  rest.post<RegisterBody>(`${API_URL}/users`, (req, _res, ctx) => {
-    const userData = req.body.user;
-    // TODO: one query?
+  rest.post<RegisterBody>(`${API_URL}/users`, async (req, _res, ctx) => {
+    // const userData = req.body.user;
+    const { user } = await req.json();
+    // OR is not supported
+    // https://github.com/mswjs/data/issues/89
+
     const userWithEmail = db.user.findFirst({
-      where: { email: { equals: userData.email } },
+      where: { email: { equals: user.email } },
     });
     const userWithUsername = db.user.findFirst({
-      where: { username: { equals: userData.username } },
+      where: { username: { equals: user.username } },
     });
-
     if (userWithEmail || userWithUsername) {
       const errorMessage = 'has already been taken';
       const errors: Record<string, [string]> = {};
-      if (userData.username === userWithUsername?.username) {
+      if (user.username === userWithUsername?.username) {
         errors['username'] = [errorMessage];
       }
-
-      if (userData.email === userWithEmail?.email) {
+      if (user.email === userWithEmail?.email) {
         errors['email'] = [errorMessage];
       }
-
       return delayedResponse(
         ctx.status(422),
         ctx.json({
@@ -48,33 +45,25 @@ export const userHandlers = [
         }),
       );
     }
-    const passwordHash = hash(userData.password);
 
-    db.user.create({
-      ...userData,
-      password: passwordHash,
-      image:
-        'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1246.jpg',
-      bio: '',
-    });
+    const { email, password } = createUser(user);
 
     persistDb('user');
-
-    const autenticatedUser = authenticate({
-      email: userData.email,
-      password: userData.password,
+    const authenticatedUser = authenticate({
+      email,
+      password,
     });
 
-    return delayedResponse(ctx.status(200), ctx.json(autenticatedUser));
+    return delayedResponse(ctx.status(200), ctx.json(authenticatedUser));
   }),
 
-  rest.post<LoginBody>(`${API_URL}/users/login`, (req, res, ctx) => {
+  rest.post<LoginBody>(`${API_URL}/users/login`, async (req, _res, ctx) => {
     try {
-      const credentials = req.body.user;
+      const { user: credentials } = await req.json();
 
-      const autenticatedUser = authenticate(credentials);
+      const authenticatedUser = authenticate(credentials);
 
-      return delayedResponse(ctx.status(200), ctx.json(autenticatedUser));
+      return delayedResponse(ctx.status(200), ctx.json(authenticatedUser));
     } catch (error) {
       return delayedResponse(
         ctx.status(403),
@@ -85,5 +74,52 @@ export const userHandlers = [
         }),
       );
     }
+  }),
+
+  rest.post(`${API_URL}/profiles/:username/follow`, (req, res, ctx) => {
+    // try {
+    //   const user = requireAuth(req);
+    //   const { username } = req.params;
+    //   const updatedUser = db.user.update({
+    //     where: { username: { equals: username } },
+    //     data: { followedBy: prev => [...prev, user] },
+    //   });
+    //   const sanitizedProfile = sanitizeProfile(updatedUser, {
+    //     following: true,
+    //   });
+    //   console.log('SANITIZED PROFILE', sanitizedProfile);
+    //   persistDb('user');
+    //   return delayedResponse(
+    //     ctx.status(200),
+    //     ctx.json({ profile: sanitizedProfile }),
+    //   );
+    // } catch (error) {
+    //   return delayedResponse(ctx.status(401), ctx.json(error));
+    // }
+  }),
+
+  rest.delete(`${API_URL}/profiles/:username/follow`, (req, res, ctx) => {
+    // try {
+    //   const user = requireAuth(req);
+    //   const { username } = req.params;
+    //   const updatedUser = db.user.update({
+    //     where: { username: { equals: username } },
+    //     data: {
+    //       followedBy: prev =>
+    //         prev.filter(({ username }) => username !== user.username),
+    //     },
+    //   });
+    //   const sanitizedProfile = sanitizeProfile(updatedUser, {
+    //     following: false,
+    //   });
+    //   console.log('SANITIZED PROFILE DELETE', sanitizedProfile);
+    //   persistDb('user');
+    //   return delayedResponse(
+    //     ctx.status(200),
+    //     ctx.json({ profile: sanitizedProfile }),
+    //   );
+    // } catch (error) {
+    //   return delayedResponse(ctx.status(401));
+    // }
   }),
 ];
