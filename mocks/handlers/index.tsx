a10d9c4cf774e99cb4cdaeab161f2271ccea80ff
rainmodred <albphoenix@gmail.com>
2022-08-13 03:48:38 +0300

    Refactor home page tests

--- mocks/handlers/index.tsx
+++ mocks/handlers/index.tsx
@@ -1,3 +1,4 @@
+import { articleHandlers } from './article';
 import { userHandlers } from './user';
 
-export const handlers = [...userHandlers];
+export const handlers = [...userHandlers, ...articleHandlers];
