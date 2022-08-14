64ad8158308da218dcbdb522e42f44c2d21b174c
rainmodred <albphoenix@gmail.com>
2022-08-15 00:34:40 +0300

    Refactor _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,46 +1,56 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+  require('../mocks');
+}
+import { useState } from 'react';
+import { AppProps } from 'next/app';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+
 import { AuthProvider } from '../context/AuthContext';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 
-if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
-  if (typeof window === 'undefined') {
-    const { server } = require('../mocks/server');
-    server.listen({ onUnhandledRequest: 'bypass' });
-  } else {
-    const { worker } = require('../mocks/browser');
-    worker.start({ onUnhandledRequest: 'bypass' });
-  }
-}
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: 1,
-    },
-  },
-});
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       refetchOnWindowFocus: false,
+//       retry: 1,
+//     },
+//   },
+// });
 
 interface AllProvidersProps {
   children: React.ReactNode;
+  pageProps: any;
 }
 
-function AllProviders({ children }: AllProvidersProps): JSX.Element {
+function AllProviders({ children, pageProps }: AllProvidersProps): JSX.Element {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: 1,
+          },
+        },
+      }),
+  );
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <Layout>{children}</Layout>
-      </AuthProvider>
+      <Hydrate state={pageProps.dehydratedState}>
+        <AuthProvider>
+          <Layout>{children}</Layout>
+        </AuthProvider>
+      </Hydrate>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AllProviders>
+    <AllProviders pageProps={pageProps}>
       <Component {...pageProps} />
     </AllProviders>
   );
