dd9c7e92d94e726dce55720e8f711deb63ecae17
rainmodred <albphoenix@gmail.com>
2022-08-14 03:09:56 +0300

    Tag page WIP

--- components/Tags/Tags.test.tsx
+++ components/Tags/Tags.test.tsx
@@ -3,39 +3,32 @@ import { render, screen } from '../../test/test-utils';
 import { waitForElementToBeRemoved } from '@testing-library/react';
 import { rest } from 'msw';
 import { server } from '../../mocks/server';
-import { apiUrl } from '../../api';
+import { API_URL } from '../../config/config';
+import db from '../../mocks/db';
 
 describe('Tags', () => {
   it('should render', async () => {
+    const tags = [
+      db.tag.create({ id: '1', name: 'first' }),
+      db.tag.create({ id: '2', name: 'second' }),
+    ];
     render(<Tags />);
 
     expect(screen.getByText(/popular tags/i)).toBeInTheDocument();
     await waitForElementToBeRemoved(screen.getByText(/loading/i));
-    expect(
-      screen.getByRole('link', {
-        name: /welcome/i,
-      }),
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole('link', {
-        name: /implementations/i,
-      }),
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole('link', {
-        name: /codebaseshow/i,
-      }),
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole('link', {
-        name: /introduction/i,
-      }),
-    ).toBeInTheDocument();
+
+    tags.forEach(tag => {
+      expect(
+        screen.getByRole('link', {
+          name: tag.name,
+        }),
+      ).toBeInTheDocument();
+    });
   });
 
   it('should show error', async () => {
     server.use(
-      rest.get(`${apiUrl}/tags`, (_req, res, ctx) => {
+      rest.get(`${API_URL}/tags`, (_req, res, ctx) => {
         return res(ctx.status(500), ctx.json({}));
       }),
     );
