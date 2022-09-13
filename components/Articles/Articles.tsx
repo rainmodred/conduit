c971cf350b60ad1d9ee4a4171e58bbaf96235744
rainmodred <albphoenix@gmail.com>
2022-09-13 07:56:07 +0300

    fixup! Refactor Articles

--- components/Articles/Articles.tsx
+++ components/Articles/Articles.tsx
@@ -1,51 +1,9 @@
-import { useRouter } from 'next/router';
-import { useQuery } from 'react-query';
-
 import ArticlePreview from './ArticlePreview/ArticlePreview';
 import FavoriteArticleButton from '../Shared/Buttons/FavoriteButton/FavoriteButton';
 import Pagination from '../Shared/Pagination/Pagination';
 import useFavoritePreviewMutation from '../../hooks/useFavoritePreviewMutation';
-import { useAuth } from '../../context/AuthContext';
-import { getArticles, getFeed } from '../../utils/api';
-import { QUERY_KEYS } from '../../utils/queryKeys';
-import { ARTICLES_LIMIT } from '../../config/config';
-import { ArticleFromApi, ArticlesFromAPi } from '../../utils/types';
 import { getTotalPages } from '../../utils/utils';
-
-function createQueryKey(
-  route: string,
-  options: { username?: string; tag?: string; page?: number },
-) {
-  const { username, page, tag } = options;
-  switch (route) {
-    case '/':
-      if (!page) {
-        throw new Error('Not implemented');
-      }
-      return QUERY_KEYS.feed(page);
-    case '/all':
-      if (!page) {
-        throw new Error('Not implemented');
-      }
-      return QUERY_KEYS.all(page);
-    case '/profile/[username]':
-      if (!page || !username) {
-        throw new Error('Not implemented');
-      }
-      return QUERY_KEYS.myArticles(username, page);
-    case '/profile/[username]/favorites':
-      if (!page) {
-        throw new Error('Not implemented');
-      }
-      return QUERY_KEYS.favorites(page);
-    case '/tag/[tag]':
-      if (!page || !tag) {
-      }
-      return QUERY_KEYS.tag(tag, page);
-    default:
-      throw new Error('Not implemented');
-  }
-}
+import { ArticlesFromAPi } from '../../utils/types';
 
 interface ArticleProps {
   queryKey: readonly [string, string, number?];
